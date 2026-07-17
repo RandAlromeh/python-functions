d214818059f644b2ca4dc1c640613b56825fc666
Rand <rand6304@gmail.com>
2026-07-17 16:19:36 +0300

    Add variables

--- hello.py
+++ hello.py
@@ -1,2 +1,10 @@
-# my first program
-print("Hello, Python!")
+name = input("Your name? ")
+age = int(input("Your age? "))
+print(f"Hi {name}, in a year you'll be {age + 1}")
+grade = int(input("Your grade? "))
+if grade >= 90:
+    print("Excellent")
+elif grade >= 60:
+    print("Pass")
+else:
+    print("Try again")
