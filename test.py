205a6d37eb9ab5860d17dba66d0ac3e4216801a7
Rand <rand6304@gmail.com>
2026-07-17 19:45:07 +0300

    g

---
+++ test.py
@@ -0,0 +1,13 @@
+def greet1(name):
+    print(f"{name} Are you Okay>")
+
+
+def greet2(name):
+    return f"{name} Are you Okay?"
+
+
+concern = greet1("Rand")
+convern = greet2("Rand")
+
+print(concern)
+print(convern)
